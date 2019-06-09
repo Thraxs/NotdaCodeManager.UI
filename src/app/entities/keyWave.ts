2b77c9bbeca99fd25b11fda4f38eae1c6bd76549
Javier Rico <javi921@hotmail.es>
2019-06-09 14:03:44 +0100

    Minor changes to medal calculator

--- src/app/entities/keyWave.ts
+++ src/app/entities/keyWave.ts
@@ -17,10 +17,10 @@ export class KeyWave {
                 this.displayName = "USS Independence arrival / Dark Clouds - Minos";
                 break;
             case KeyWaveType.Wave12:
-                this.displayName = "Revelation CAS / Delta chain - ammo spawn / Nightstalker class As"
+                this.displayName = "Revelation CAS / Delta chaingun or ammo spawn / Nightstalker class As"
                 break;
             case KeyWaveType.Wave15:
-                this.displayName = "Fox transmission / Golf transmission / BHD refuel";
+                this.displayName = "Fox transmission / Golf transmission / BHD refuel transmission";
                 break;
         }
     }
